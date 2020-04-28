91fd742c8496045b210094a61e59a4b980a92a9c
V0l9emar <63954046+V0l9emar@users.noreply.github.com>
2020-04-28 17:05:54 +0400

    secondChanges

--- Section_1/19_Calback Funkcii/calback.js
+++ Section_1/19_Calback Funkcii/calback.js
@@ -1,9 +1,9 @@
 //Funkcii v JS yavlayutsa obyektami;
-function foo(){
-  console.log("hello Foo!")
+function foo() {
+  console.log("hello Foo!");
 }
 foo();
-foo.field = 'Voldemar';
+foo.field = "Voldemar";
 console.log(foo.field);
 
 //funkcii v JS nazivayutsa funkciyami pervogo klassa:  oni mogut bit peredanni v kachestve paramtrov v drugiye funkcii, mogut bit prisvoyeni v peremenniye, s nimi mojno proizvodit manipulacii kak i s drugimi tipami dannix
@@ -11,23 +11,23 @@ console.log(foo.field);
 //--------------------------------------------------------//
 //Funkcii visshego poradka, eto funkcii kotoriye prinimayut v kachestve parametrov drugiye funkcii ili vozvrashayut funkcii
 
-//metod map: vozvrashayet noviy massiv sostoyashiy iz rezultatov vizova funkcii calbacka kotoraya bila peredana v kachestve parametra i vozvrashayet noviy massiv iz rezultata vizova nashey funkcii, eto nujno dla sokrasheniya kolichestva koda 
+//metod map: vozvrashayet noviy massiv sostoyashiy iz rezultatov vizova funkcii calbacka kotoraya bila peredana v kachestve parametra i vozvrashayet noviy massiv iz rezultata vizova nashey funkcii, eto nujno dla sokrasheniya kolichestva koda
 
 const arr = ["Meku", "Nadir", "Ibo", "Sara", "Mery", "Voldemar"];
 //poluchit - [4, 5, 3, 4, 4, 8];
 let newArr = [];
-for(i=0; i<arr.length; i++){
+for (i = 0; i < arr.length; i++) {
   newArr.push(arr[i].length);
-}console.log(newArr);
+}
+console.log(newArr);
 
 let newArr2 = [];
-for(i=0; i<arr.length; i++){
+for (i = 0; i < arr.length; i++) {
   newArr2.push(arr[i].toUpperCase());
-}console.log(newArr2);
+}
+console.log(newArr2);
 console.log(newArr);
 //#######zadacah vipolnena pravilno no mnogo raznix povtorov#######//
-
-
 
 //1.primem 2 parametra 'arr' i 'fn'
 //2.vnutri seba sozdayet noviy massiv;
@@ -39,26 +39,29 @@ console.log(newArr);
 //8.Zapisivaem v peremennuyu resultat massiva
 //9.Chtob poluchi toUpperCase
 const names = ["Ayshan", "Shamsi", "Sara", "Hacar", "Voldemar"];
-function mapArray(arr, fn){//1
+function mapArray(arr, fn) {
+  //1
   const res = []; //2
-  for(i=0; i<arr.length; i++){//3
-    res.push(fn(arr[i]));//4
+  for (i = 0; i < arr.length; i++) {
+    //3
+    res.push(fn(arr[i])); //4
   }
   return res;
-};
+}
 //5
-function nameLength(el){//6
+function nameLength(el) {
+  //6
   //console.log(el);
   return el.length;
-};
+}
 //7
 //mapArray(names, nameLength);
 //massiv imen popadaet v mapArray i tuda je popadaet funkciya, ona pod imenem 'fn', pereberayem massiv #3, dalee na #4 pri kajdoy iteracii pushitsa odin element massiva
 //9
-function nameToUpperCase(el){
+function nameToUpperCase(el) {
   return el.toUpperCase();
-};
-//8 
+}
+//8
 const result = mapArray(names, nameLength);
 //10
 const result2 = mapArray(names, nameToUpperCase);
@@ -68,48 +71,75 @@ console.log(result2);
 
 //######### Vozvrasheniye funkciy iz drugix funkciy ###########//
 
-function greeting(firstName){
-  return function(lastName){
+function greeting(firstName) {
+  return function (lastName) {
     return `Hello, ${firstName} ${lastName}`;
   };
 }
 //const testGreeting = greeting("Voldemar");
 //const fullName = testGreeting("Turovskiy");
-const fullName = greeting("Voldemar")("Turovskiy")
+const fullName = greeting("Voldemar")("Turovskiy");
 console.log(fullName);
 
 //#############################################################//
 
-function question(job){
-  if(job === 'developer'){
-    return function(name){
+function question(job) {
+  if (job === "developer") {
+    return function (name) {
       return `${name}, что такое JavaScript?`;
     };
-  } else if(job === 'teacher'){
-    return function(name){
+  } else if (job === "teacher") {
+    return function (name) {
       return `${name}, какой предмет вы ведете?`;
     };
   }
 }
 
-const developerQuestion = question('developer');
-console.log(developerQuestion('Voldemar'));
-const teacherQuestion = question('teacher');
-console.log(teacherQuestion('Voldemar'));
+const developerQuestion = question("developer");
+console.log(developerQuestion("Voldemar"));
+const teacherQuestion = question("teacher");
+console.log(teacherQuestion("Voldemar"));
 
 //################# Uprostim danniy primer #####################//
 
-function question(job){
-  const jobDictionary = { //sozdayem obyekt
-    developer: 'что такое JavaScript',
-    teacher: 'какой предмет вы ведете'
+function question(job) {
+  const jobDictionary = {
+    //sozdayem obyekt
+    developer: "что такое JavaScript?",
+    teacher: "какой предмет вы ведете?",
     //takim obrazom mojno rashirat spisok, dobaviv v slovar
   };
-  return function(name){
+  return function (name) {
     return `${name}, ${jobDictionary[job]}?`;
   };
 }
-const developeQuestion = question('developer');
-console.log(developeQuestion('Voldemar'));
-const teacheQuestion = question('teacher');
-console.log(teacheQuestion('Voldemar'));
+const developeQuestion = question("developer");
+console.log(developeQuestion("Voldemar"));
+const teacheQuestion = question("teacher");
+console.log(teacheQuestion("Voldemar"));
+
+//#############################################################//
+function doWithLoading(fn) {
+  console.log("начало загрузки");
+  let returnValue = fn();
+  console.log("конец загрузки");
+  return returnValue;
+}
+function process() {
+  console.log("загружаем...");
+}
+doWithLoading(process);
+
+//########################### Zamikaniye ####################################//
+function createGenerator(prefix) {
+  let index = 0;
+  return function generateNewID() {
+    index++;
+    return prefix + index.toString();
+  };
+}
+let generateNewID = createGenerator("Вызов номер: ");
+console.log(generateNewID());
+console.log(generateNewID());
+console.log(generateNewID());
+console.log(generateNewID());
