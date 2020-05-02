ec8f7012f0d4a2074a2d9b456c10020c7040b480
V0l9emar <63954046+V0l9emar@users.noreply.github.com>
2020-05-02 12:44:11 +0400

    this

--- Section_1/21_this/this.js
+++ Section_1/21_this/this.js
@@ -4,15 +4,17 @@ function getThis() {
 }
 getThis(); // ssilayetsa na globalniy obyekt window{}
 window.getThis();
-//###############################################################//
 
-function getPrice() {
-  console.log(this.price);
+//################ funkciya v kachestve metodov ###################//
+
+function getPrice(currency = "$") {
+  //dobavl argument "currency" i prisv default znacheniye
+  console.log(currency + this.price);
   return this;
 }
 function getName() {
   console.log(this.name);
-  return this; //posle togo kak return this poyavilsa, problema s vizovom v prod3 reshilas
+  return this; //posle togo kak "return this" poyavilsa, problema s vizovom v prod3 reshilas
 }
 const prod1 = {
   name: "Intel",
@@ -30,8 +32,9 @@ const prod1 = {
     },
   },
 };
-prod1.getPrice();
+
 prod1.info.getInfo(); //kontext vizova opredelayetsa pri vizove funkcii
+prod1.getPrice();
 prod1.getName();
 
 //getPrice(); vixod- undefined, po tomu chto ona v globalnoy srede i u window net takogo svoystva, poetomu ee mojno otpravit v obj prod1 vishe, vmesto getPrice funkcii.
@@ -58,6 +61,24 @@ const prod3 = {
 };
 prod3
   .getName() //undefined pri vizove
-  .getPrice(); //oshibka, u undefined net undefined.getPrice(), poetomu nujno v eti funkcii return this
+  .getPrice(); //oshibka, u undefined net undefined.getPrice(), nujno dobavit v eti funkcii return this
 
 //######################## Potera kontexta ##########################//
+//Vozmojnost vizivat funkciyu s opredelennim kontextom
+const prod4 = {
+  name: "Celeron",
+  price: 140,
+  getPrice, //otnos k potere kontexta
+};
+getName.call(prod4);
+getPrice.call(prod4, "*"); // takim obrzom mojno peredavat eshe argumenti (vmesto defaul currency $)
+//pervim obazat doljen idti context vizova(prod4), esli bez nego to doljen bit ukazan null
+//apply prinimayet peredavayemiye metodi v kachestve massiva v otlichaye ot call
+getPrice.apply(prod4, ["*"]);
+
+// potera kontexta proisxodit obichno kogda mi peredayem metodi vnutr drugix funkciy, kotoriye budut vizvani v ramkax drugogo kontexta.
+
+const getPriceBind = prod4.getPrice.bind(prod4, "^"); //bind ne vizivayet funkciyu a vozvrashayet etu je funkciyu s privazannim kontextom
+//console.log(getPriceBind)
+
+setTimeout(getPriceBind, 1000);
